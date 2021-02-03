6cb331e07239c91bff45eef4a80188e18c25dda4
gopetracca <gabriel.petracca@telefonica.com>
2021-02-03 19:06:22 +0100

    harden

--- mirror.sh
+++ mirror.sh
@@ -18,8 +18,8 @@ echo "ls -al\n: $(ls -al)"
 # Git options
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa_di_codecommit -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 
-# Make sure remote "mirror" does not exist
-git remote rm mirror
+# Make sure remote "mirror" does not exist. Ignore error if not exists
+git remote rm mirror || true
 
 # Add remote mirror
 git remote add mirror "$INPUT_TARGET_REPO_URL"
