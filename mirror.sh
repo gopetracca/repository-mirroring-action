cdf2dd910e521c4fd290fd096d1a1ae860022a75
gopetracca <gabriel.petracca@telefonica.com>
2021-02-03 18:36:55 +0100

    harden

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,14 @@ set -eu
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+# Git options
+export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa_di_codecommit -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+
+# Make sure remote "mirror" does not exist
+git remote rm mirror
+
+# Add remote mirror
 git remote add mirror "$INPUT_TARGET_REPO_URL"
+
+# Push
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
